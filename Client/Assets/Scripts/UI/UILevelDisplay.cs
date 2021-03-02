ef2e9da5cb6c3f4a792d43a6625f72e59d87032c
Voraque <Dnicholas329@gmail.com>
2021-03-01 22:02:14 -0800

    Updated UI Displays

---
+++ Client/Assets/Scripts/UI/UILevelDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Client;
+using Client.Game;
+using TMPro;
+
+public class UILevelDisplay : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI levelText;
+
+    void Start() {
+        StaticManager.GameClient.UpdatePlayerInfo += DisplayPlayerLevel;
+    }
+
+    private void DisplayPlayerLevel(UpdatePlayerInfoPacket packet) {
+        levelText.text = "Level: " + packet.Level;
+    }
+}
---
+++ Client/Assets/Scripts/UI/UIXpDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Client;
+using Client.Game;
+using TMPro;
+
+public class UIXpDisplay : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI xpText;
+
+    void Start() {
+        StaticManager.GameClient.UpdatePlayerInfo += DisplayXPLevel;
+    }
+
+    private void DisplayXPLevel(UpdatePlayerInfoPacket packet) {
+        xpText.text = "XP: " + packet.XP + "/" + DisplayExperienceToLevel(packet.Level);
+    }
+
+    private int DisplayExperienceToLevel(int currentLevel) {
+        return currentLevel * 5; ;
+    }
+}
+
