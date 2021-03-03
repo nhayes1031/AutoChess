a620958966e6cdb6f441dcbac09863fd20fbbb04
Voraque <Dnicholas329@gmail.com>
2021-03-02 21:35:05 -0800

    UI Display Updates

---
+++ Client/Assets/Scripts/UI/UIGoldDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Client;
+using Client.Game;
+using TMPro;
+
+public class UIGoldDisplay : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI goldText;
+
+    void Start() {
+        StaticManager.GameClient.UpdatePlayerInfo += DisplayPlayerGold;
+    }
+
+    private void DisplayPlayerGold(UpdatePlayerInfoPacket packet) {
+        goldText.text = "Gold: " + packet.Gold;
+    }
+}
