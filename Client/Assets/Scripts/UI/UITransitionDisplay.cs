234441848886309df0bd156e700f6c637793c0a5
Voraque <Dnicholas329@gmail.com>
2021-03-07 17:40:35 -0800

    Added game phase display which was mistakingly excluded from the prior commit.

---
+++ Client/Assets/Scripts/UI/UITransitionDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Client;
+using Client.Game;
+using TMPro;
+
+public class UITransitionDisplay : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI transitionText = null;
+
+    private void Start() {
+        StaticManager.GameClient.TransitionUpdate += DisplayTransitionUpdate;
+        StaticManager.GameClient.TransitionUpdate += ReadTransitionPacket;
+    }
+
+    private void DisplayTransitionUpdate(TransitionUpdatePacket packet) {
+        transitionText.text = packet.Event;
+    }
+
+    private void ReadTransitionPacket(TransitionUpdatePacket packet) {
+        
+    }
+}
